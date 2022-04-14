ef70636f0ca73dea8461186b25e8f9daac6731fb
Elise Ooi <71936066+eliseooi@users.noreply.github.com>
2022-04-14 15:42:38 -0700

    Updated connection.js
    added if else condition to each intent

--- routes/connection.js
+++ routes/connection.js
@@ -6,35 +6,51 @@ const calendarURL = 'https://rest.gohighlevel.com/v1/calendars/teams';
 const gohighlevelBearerToken = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2NhdGlvbl9pZCI6Imo4ZVdaU2ZObndSUUhkakxkVkN2IiwiY29tcGFueV9pZCI6IkJrUzZTUTF2T0k3NmQxU2ZvaW42IiwidmVyc2lvbiI6MSwiaWF0IjoxNjQ2OTgwNTMxNzk1LCJzdWIiOiJJTVhLUENlRndNQXlVNUc3TmV5TSJ9.JO8_nFUPZwyVnEd46lerAr909TXte-X4Di9iOdGq99M';
 
 router.post('/', function(req, res, next) {
+  var intent = req.body.queryResult.intent.displayName;
 
-  axios.get(locationURL,{ headers: { 'Authorization': gohighlevelBearerToken} }).then(aRes => {
-    let address = aRes.data.locations[0].address;
-    let city = aRes.data.locations[0].city;
-    let state = aRes.data.locations[0].state;
-    let country = aRes.data.locations.country;
+  if (intent == "locationInfo"){
+    axios.get(locationURL,{ headers: { 'Authorization': gohighlevelBearerToken} }).then(aRes => {
+      let address = aRes.data.locations[0].address;
+      let city = aRes.data.locations[0].city;
+      let state = aRes.data.locations[0].state;
+      let country = aRes.data.locations[0].country;
+  
+      let textResponse = `The studio is located at ${address}, ${city}, ${state}, ${country}.`;
+      res.send(createTextResponse(textResponse));
+  
+    }).catch(err => {
+      console.log(err);
+    })
+  }
 
-    let textResponse = `The studio is located at ${address}, ${city}, ${state}, ${country}.`;
-    res.send(createTextResponse(textResponse));
-
-  }).catch(err => {
-    console.log(err);
-  })
+  else if (intent == "testIntent"){
+    axios.get(calendarURL,{ headers: { 'Authorization': gohighlevelBearerToken} }).then(aRes => {
+      let apptLink = aRes.data.teams[0].calendarConfig.link;
+  
+      let textResponse = `The link to make an appointment is ${apptLink}.`;
+      res.send(createTextResponse(textResponse));
+  
+    }).catch(err => {
+      console.log(err);
+    })
+  }
 
 });
 
-router.post('/', function(req, res, next) {
+// router.post('/', function(req, res, next) {
 
-  axios.get(calendarURL,{ headers: { 'Authorization': gohighlevelBearerToken} }).then(aRes => {
-    let apptLink = aRes.data.teams[0].calendarConfig.link;
+//   axios.get(calendarURL,{ headers: { 'Authorization': gohighlevelBearerToken} }).then(aRes => {
+//     let apptLink = aRes.data.teams[0].calendarConfig.link;
 
-    let textResponse = `The link to make an appointment is ${apptLink}.`;
-    res.send(createTextResponse(textResponse));
+//     let textResponse = `The link to make an appointment is ${apptLink}.`;
+//     res.send(createTextResponse(textResponse));
 
-  }).catch(err => {
-    console.log(err);
-  })
+//   }).catch(err => {
+//     console.log(err);
+//   })
 
-});
+// });
+
 
 function createTextResponse(textResponse){
   let response = {
